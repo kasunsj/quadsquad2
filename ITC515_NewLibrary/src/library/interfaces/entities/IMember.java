0312fcfbdb3755b24903f0086e017160fb8606a2
ishacharan <ishacharan@gmail.com>
2016-10-04 13:58:57 +1100

    defining boolean methods

--- ITC515_NewLibrary/src/library/interfaces/entities/IMember.java
+++ ITC515_NewLibrary/src/library/interfaces/entities/IMember.java
@@ -13,15 +13,20 @@ public interface IMember {
 	//static final float variable 
 	public static final float FINE_LIMIT = 10.0f;	
 	
-	
+	//boolean methods
+	//defining hasOverDueLoans method
 	public boolean hasOverDueLoans();
 	
+	//defining hasReachedLoanLimit method
 	public boolean hasReachedLoanLimit();
 	
+	//defining hasFinesPayable
 	public boolean hasFinesPayable();
 	
+	//defining hasReachedFineLimit
 	public boolean hasReachedFineLimit();
 	
+	//
 	public float   getFineAmount();
 	
 	public void    addFine(float fine);
