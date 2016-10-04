4263ce189de91b42f11bbc0872d37bb0be1457dc
ishacharan <ishacharan@gmail.com>
2016-10-04 13:49:26 +1100

    comment

--- ITC515_NewLibrary/src/library/interfaces/entities/EBookState.java
+++ ITC515_NewLibrary/src/library/interfaces/entities/EBookState.java
@@ -1,7 +1,7 @@
 //EBookState in the respective package
 package library.interfaces.entities;
 
-
+//enum method has been created 
 public enum EBookState {
 	AVAILABLE, ON_LOAN, LOST, DAMAGED, DISPOSED
 
