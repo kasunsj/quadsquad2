28911648a979b90c35dba66d4625dc54c333599b
ishacharan <ishacharan@gmail.com>
2016-10-08 08:18:23 +1100

    method

--- ITC515_NewLibrary/src/library/interfaces/hardware/IScannerListener.java
+++ ITC515_NewLibrary/src/library/interfaces/hardware/IScannerListener.java
@@ -4,6 +4,7 @@ package library.interfaces.hardware;
 //IScannerListener interface has been created
 public interface IScannerListener {
 	
+	//no return type method with integer parameter has been created
 	public void bookScanned(int barcode);
 
 }
