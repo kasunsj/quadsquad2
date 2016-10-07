042ecc5b133e8f8767f75a653590bd6de9ccb754
ishacharan <ishacharan@gmail.com>
2016-10-08 09:13:58 +1100

    displaying loan limit

--- ITC515_NewLibrary/src/library/panels/borrow/ABorrowPanel.java
+++ ITC515_NewLibrary/src/library/panels/borrow/ABorrowPanel.java
@@ -37,7 +37,7 @@ public abstract class ABorrowPanel extends JPanel implements IBorrowUI {
 		throw new RuntimeException("Illegal operation in current state");//throwing exception	
 	}
 
-	
+	//displaying loan limit
 	@Override
 	public void displayAtLoanLimitMessage() {
 		throw new RuntimeException("Illegal operation in current state");//throwing runtime exception	
