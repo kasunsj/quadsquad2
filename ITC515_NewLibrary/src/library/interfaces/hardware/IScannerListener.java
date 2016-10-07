cbff33909bd9cb445196a4e27a07177f9af7ab37
ishacharan <ishacharan@gmail.com>
2016-10-08 08:17:41 +1100

    IScannerListener

--- ITC515_NewLibrary/src/library/interfaces/hardware/IScannerListener.java
+++ ITC515_NewLibrary/src/library/interfaces/hardware/IScannerListener.java
@@ -1,6 +1,7 @@
 //package where the IScannerListener.java exists
 package library.interfaces.hardware;
 
+//IScannerListener interface has been created
 public interface IScannerListener {
 	
 	public void bookScanned(int barcode);
