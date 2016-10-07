c774c8f0a07918d350337c139c3b6bd14da7c3d9
ishacharan <ishacharan@gmail.com>
2016-10-08 08:06:25 +1100

    interface

--- ITC515_NewLibrary/src/library/interfaces/hardware/ICardReaderListener.java
+++ ITC515_NewLibrary/src/library/interfaces/hardware/ICardReaderListener.java
@@ -1,7 +1,10 @@
+//package where ICardReaderListenre located
 package library.interfaces.hardware;
 
+//an interface ICardReaderListenenr has been created
 public interface ICardReaderListener {
 	
+	//cardswip 
 	public void cardSwiped(int cardData);
 
 }
