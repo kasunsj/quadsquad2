bad548f45605d4fdddbd27725f6f9e82f85dbdb5
ishacharan <ishacharan@gmail.com>
2016-10-08 08:15:45 +1100

    boolean parameter

--- ITC515_NewLibrary/src/library/interfaces/hardware/IScanner.java
+++ ITC515_NewLibrary/src/library/interfaces/hardware/IScanner.java
@@ -7,6 +7,7 @@ public interface IScanner {
 	//adding listener method wiTH IScannerListener parameter
 	public void addListener(IScannerListener listener);
 	
+	//no return type method with boolean parameter
 	public void setEnabled(boolean enabled);
 
 }
