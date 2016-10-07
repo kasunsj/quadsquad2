8697492846c8e4097ec1770192fd9f97b3f4213d
ishacharan <ishacharan@gmail.com>
2016-10-08 07:50:11 +1100

    enum

--- ITC515_NewLibrary/src/library/interfaces/entities/EMemberState.java
+++ ITC515_NewLibrary/src/library/interfaces/entities/EMemberState.java
@@ -1,6 +1,7 @@
 //package in which the EBookState exists
 package library.interfaces.entities;
 
+//enum method to state member has been created
 public enum EMemberState {
 
 	BORROWING_ALLOWED, BORROWING_DISALLOWED
