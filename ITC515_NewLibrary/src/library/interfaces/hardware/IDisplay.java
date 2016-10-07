fcdda791d471818690d26932b62e273d405013b3
ishacharan <ishacharan@gmail.com>
2016-10-08 08:08:25 +1100

    javax

--- ITC515_NewLibrary/src/library/interfaces/hardware/IDisplay.java
+++ ITC515_NewLibrary/src/library/interfaces/hardware/IDisplay.java
@@ -1,6 +1,7 @@
 //package where the IDisplay.java exists has been shown
 package library.interfaces.hardware;
 
+//importing javax's swing package
 import javax.swing.JPanel;
 
 public interface IDisplay {
