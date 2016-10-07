dc8468a66032cfc3c0afb67649d32ba38261b849
ishacharan <ishacharan@gmail.com>
2016-10-08 08:13:31 +1100

    IScanner interface

--- ITC515_NewLibrary/src/library/interfaces/hardware/IScanner.java
+++ ITC515_NewLibrary/src/library/interfaces/hardware/IScanner.java
@@ -1,6 +1,7 @@
 //packageIScanner.java is there
 package library.interfaces.hardware;
 
+//ISacnner interface created
 public interface IScanner {
 	
 	public void addListener(IScannerListener listener);
